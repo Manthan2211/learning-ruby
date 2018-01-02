cf7da91f5e6132c2c9b3138fa035d77cbd3621f2
Manthan2211 <manthanandharia@gmail.com>
2018-01-02 17:44:56 +0530

    parsing using csv

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,4 +1,11 @@
-lines = File.readlines "event_attendees.csv"
-lines.each do |line|
-  puts line
+#lines = File.readlines "event_attendees.csv"
+#lines.each do |line|
+#  puts line
+#end
+require 'csv'
+contents = CSV.open "event_attendees.csv", headers: true,header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+  puts "#{name} #{zipcode}"
 end
